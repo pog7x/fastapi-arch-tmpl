ec8351e4fe396e2e96a41ac1cc1262fd0a5db8bc
pog7x <poluningm@gmail.com>
2023-02-03 15:53:36 +0300

    load_testing

---
+++ locustfile.py
@@ -0,0 +1,88 @@
+import json
+import random
+
+from locust import FastHttpUser, between, task
+
+from tests.factories import CoffeeFactory, UserFactory
+
+
+class CoffeeUser(FastHttpUser):
+    wait_time = between(1, 3)
+
+    @task(2)
+    def create_coffee(self):
+        self.client.post(
+            url="/coffee/",
+            json=json.loads(CoffeeFactory().build().json()),
+            name="create_coffee",
+        )
+
+    @task
+    def search_coffee(self):
+        self.client.get(
+            url="/coffee/",
+            name="search_coffee",
+        )
+
+    @task
+    def get_coffee_by_id(self):
+        self.client.get(
+            url=f"/coffee/{random.randint(1, 100)}",
+            name="get_coffee_by_id",
+        )
+
+    @task
+    def update_coffee(self):
+        self.client.put(
+            url=f"/coffee/{random.randint(1, 100)}",
+            json=json.loads(CoffeeFactory().build().json(exclude_unset=True)),
+            name="update_coffee",
+        )
+
+    @task
+    def delete_coffee(self):
+        self.client.delete(
+            url=f"/coffee/{random.randint(1, 10000)}",
+            name="delete_coffee",
+        )
+
+
+class UsersUser(FastHttpUser):
+    wait_time = between(1, 3)
+
+    @task(2)
+    def create_user(self):
+        self.client.post(
+            url="/users/",
+            json=json.loads(UserFactory().build().json()),
+            name="create_user",
+        )
+
+    @task
+    def search_users(self):
+        self.client.get(
+            url="/users/",
+            name="search_users",
+        )
+
+    @task
+    def get_user_by_id(self):
+        self.client.get(
+            url=f"/users/{random.randint(1, 100)}",
+            name="get_user_by_id",
+        )
+
+    @task
+    def update_user(self):
+        self.client.put(
+            url=f"/users/{random.randint(1, 100)}",
+            json=json.loads(UserFactory().build().json(exclude_unset=True)),
+            name="update_user",
+        )
+
+    @task
+    def delete_user(self):
+        self.client.delete(
+            url=f"/users/{random.randint(1, 10000)}",
+            name="delete_user",
+        )
--- tests/factories.py
+++ tests/factories.py
@@ -1,13 +1,10 @@
 from collections import deque
-from typing import Any, Callable, Dict, List, TypeVar
+from typing import Any, Callable, Dict, TypeVar
 
 from faker import Faker
 from pydantic import BaseModel
-from pydantic_factories import AsyncPersistenceProtocol, Ignore, ModelFactory
+from pydantic_factories import Ignore, ModelFactory
 
-from app.core.session import async_session
-from app.models.coffee import Coffee
-from app.models.user import User
 from app.schemas import CoffeeModel, UserModel
 
 fake = Faker()
@@ -15,32 +12,8 @@ fake = Faker()
 T = TypeVar("T", bound=BaseModel)
 
 
-class AsyncPersistenceHandler(AsyncPersistenceProtocol[T]):
-    def __init__(self, model_class):
-        self._model_class = model_class
-
-    async def save(self, data: T) -> T:
-        async with async_session.begin() as session:
-            item = self._model_class(**data.dict())
-            session.add(item)
-            return item
-
-    async def save_many(self, data: List[T]) -> List[T]:
-        if len(data) == 0:
-            return data
-
-        async with async_session.begin() as session:
-            for item in data:
-                item = self._model_class(**data.dict())
-                session.add(item)
-
-            return data
-
-
 class CustomFactory(ModelFactory):
     __faker__ = fake
-
-    id = Ignore()
 
     @classmethod
     def get_provider_map(cls) -> Dict[Any, Callable]:
@@ -61,11 +34,12 @@ class CustomFactory(ModelFactory):
 
 class CoffeeFactory(CustomFactory):
     __model__ = CoffeeModel
-    __async_persistence__ = AsyncPersistenceHandler(Coffee)
+
+    id = Ignore()
 
 
 class UserFactory(CustomFactory):
     __model__ = UserModel
-    __async_persistence__ = AsyncPersistenceHandler(User)
 
+    id = Ignore()
     coffee_id = Ignore()
